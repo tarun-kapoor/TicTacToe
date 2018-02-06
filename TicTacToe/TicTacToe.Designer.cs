190cc30995a48480e59bb298e58f93032cfa1bc3
tkapoor <tkapoor@TKAPOOR>
2018-02-06 16:51:13 -0500

    Add project files.

---
+++ TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace TicTacToe
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxTop1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMid1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLow3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTop3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLow1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMid3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTop2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLow2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMid2 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLow3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTop3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLow1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMid3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLow2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMid2)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxTop1
+            // 
+            this.pictureBoxTop1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxTop1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxTop1.InitialImage = null;
+            this.pictureBoxTop1.Location = new System.Drawing.Point(8, 9);
+            this.pictureBoxTop1.Name = "pictureBoxTop1";
+            this.pictureBoxTop1.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxTop1.TabIndex = 0;
+            this.pictureBoxTop1.TabStop = false;
+            this.pictureBoxTop1.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxTop1.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxMid1
+            // 
+            this.pictureBoxMid1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxMid1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxMid1.Location = new System.Drawing.Point(8, 64);
+            this.pictureBoxMid1.Name = "pictureBoxMid1";
+            this.pictureBoxMid1.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxMid1.TabIndex = 1;
+            this.pictureBoxMid1.TabStop = false;
+            this.pictureBoxMid1.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxMid1.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxLow3
+            // 
+            this.pictureBoxLow3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxLow3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxLow3.Location = new System.Drawing.Point(132, 119);
+            this.pictureBoxLow3.Name = "pictureBoxLow3";
+            this.pictureBoxLow3.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxLow3.TabIndex = 2;
+            this.pictureBoxLow3.TabStop = false;
+            this.pictureBoxLow3.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxLow3.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxTop3
+            // 
+            this.pictureBoxTop3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxTop3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxTop3.Location = new System.Drawing.Point(132, 9);
+            this.pictureBoxTop3.Name = "pictureBoxTop3";
+            this.pictureBoxTop3.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxTop3.TabIndex = 3;
+            this.pictureBoxTop3.TabStop = false;
+            this.pictureBoxTop3.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxTop3.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxLow1
+            // 
+            this.pictureBoxLow1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxLow1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxLow1.Location = new System.Drawing.Point(8, 119);
+            this.pictureBoxLow1.Name = "pictureBoxLow1";
+            this.pictureBoxLow1.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxLow1.TabIndex = 4;
+            this.pictureBoxLow1.TabStop = false;
+            this.pictureBoxLow1.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxLow1.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxMid3
+            // 
+            this.pictureBoxMid3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxMid3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxMid3.Location = new System.Drawing.Point(132, 64);
+            this.pictureBoxMid3.Name = "pictureBoxMid3";
+            this.pictureBoxMid3.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxMid3.TabIndex = 5;
+            this.pictureBoxMid3.TabStop = false;
+            this.pictureBoxMid3.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxMid3.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxTop2
+            // 
+            this.pictureBoxTop2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxTop2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxTop2.InitialImage = null;
+            this.pictureBoxTop2.Location = new System.Drawing.Point(70, 9);
+            this.pictureBoxTop2.Name = "pictureBoxTop2";
+            this.pictureBoxTop2.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxTop2.TabIndex = 6;
+            this.pictureBoxTop2.TabStop = false;
+            this.pictureBoxTop2.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxTop2.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxLow2
+            // 
+            this.pictureBoxLow2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxLow2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxLow2.Location = new System.Drawing.Point(70, 119);
+            this.pictureBoxLow2.Name = "pictureBoxLow2";
+            this.pictureBoxLow2.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxLow2.TabIndex = 7;
+            this.pictureBoxLow2.TabStop = false;
+            this.pictureBoxLow2.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxLow2.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxMid2
+            // 
+            this.pictureBoxMid2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxMid2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxMid2.Location = new System.Drawing.Point(70, 64);
+            this.pictureBoxMid2.Name = "pictureBoxMid2";
+            this.pictureBoxMid2.Size = new System.Drawing.Size(56, 50);
+            this.pictureBoxMid2.TabIndex = 8;
+            this.pictureBoxMid2.TabStop = false;
+            this.pictureBoxMid2.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBoxMid2.DoubleClick += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 76);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 180);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.pictureBoxMid2);
+            this.Controls.Add(this.pictureBoxLow2);
+            this.Controls.Add(this.pictureBoxTop2);
+            this.Controls.Add(this.pictureBoxMid3);
+            this.Controls.Add(this.pictureBoxLow1);
+            this.Controls.Add(this.pictureBoxTop3);
+            this.Controls.Add(this.pictureBoxLow3);
+            this.Controls.Add(this.pictureBoxMid1);
+            this.Controls.Add(this.pictureBoxTop1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(214, 219);
+            this.MinimumSize = new System.Drawing.Size(214, 219);
+            this.Name = "FrmMain";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "TicTacToe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLow3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTop3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLow1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMid3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLow2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMid2)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxTop1;
+        private System.Windows.Forms.PictureBox pictureBoxMid1;
+        private System.Windows.Forms.PictureBox pictureBoxLow3;
+        private System.Windows.Forms.PictureBox pictureBoxTop3;
+        private System.Windows.Forms.PictureBox pictureBoxLow1;
+        private System.Windows.Forms.PictureBox pictureBoxMid3;
+        private System.Windows.Forms.PictureBox pictureBoxTop2;
+        private System.Windows.Forms.PictureBox pictureBoxLow2;
+        private System.Windows.Forms.PictureBox pictureBoxMid2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class FrmMain : Form
+    {
+        bool bCross = true;
+        string _MainText = "TicTacToe";
+        
+        public FrmMain()
+        {
+            InitializeComponent();
+            fnClear();            
+        }
+
+        private void fnClear()
+        {
+            //Next turn
+            Text = _MainText + " -Cross";
+
+            //Clear picture box
+            pictureBoxLow1.InitialImage = null;
+            pictureBoxLow2.InitialImage = null;
+            pictureBoxLow3.InitialImage = null;
+            pictureBoxMid1.InitialImage = null;
+            pictureBoxMid2.InitialImage = null;
+            pictureBoxMid3.InitialImage = null;
+            pictureBoxTop1.InitialImage = null;
+            pictureBoxTop2.InitialImage = null;
+            pictureBoxTop3.InitialImage = null;
+
+            pictureBoxLow1.Image = null;
+            pictureBoxLow2.Image = null;
+            pictureBoxLow3.Image = null;
+            pictureBoxMid1.Image = null;
+            pictureBoxMid2.Image = null;
+            pictureBoxMid3.Image = null;
+            pictureBoxTop1.Image = null;
+            pictureBoxTop2.Image = null;
+            pictureBoxTop3.Image = null;
+
+            pictureBoxLow1.ImageLocation = null;
+            pictureBoxLow2.ImageLocation = null;
+            pictureBoxLow3.ImageLocation = null;
+            pictureBoxMid1.ImageLocation = null;
+            pictureBoxMid2.ImageLocation = null;
+            pictureBoxMid3.ImageLocation = null;
+            pictureBoxTop1.ImageLocation = null;
+            pictureBoxTop2.ImageLocation = null;
+            pictureBoxTop3.ImageLocation = null;
+
+            pictureBoxLow1.Enabled = true;
+            pictureBoxLow2.Enabled = true;
+            pictureBoxLow3.Enabled = true;
+            pictureBoxMid1.Enabled = true;
+            pictureBoxMid2.Enabled = true;
+            pictureBoxMid3.Enabled = true;
+            pictureBoxTop1.Enabled = true;
+            pictureBoxTop2.Enabled = true;
+            pictureBoxTop3.Enabled = true;
+        }
+
+        private void fnDisableGame()
+        {
+            pictureBoxLow1.Enabled = false;
+            pictureBoxLow2.Enabled = false;
+            pictureBoxLow3.Enabled = false;
+            pictureBoxMid1.Enabled = false;
+            pictureBoxMid2.Enabled = false;
+            pictureBoxMid3.Enabled = false;
+            pictureBoxTop1.Enabled = false;
+            pictureBoxTop2.Enabled = false;
+            pictureBoxTop3.Enabled = false;
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            //Get control - Apply picture based on next in list
+            fnUpdateImage(sender as PictureBox);
+        }
+
+        private void fnUpdateImage(PictureBox pictureBox)
+        {
+            if (bCross)
+            {
+                pictureBox.ImageLocation = (pictureBox.ImageLocation == null) ? @"Images\Cross.png" : pictureBox.ImageLocation;
+                bCross = false;
+                Text = _MainText + " -Zero";
+            }
+            else
+            {
+                pictureBox.ImageLocation = (pictureBox.ImageLocation == null) ? @"Images\Zero.png" : pictureBox.ImageLocation;
+                bCross = true;
+                Text = _MainText + " -Cross";
+            }
+
+            pictureBox.Update();
+
+            Task.Run(() => CheckWinner());
+        }
+
+        private void CheckWinner()
+        {
+            bool bNoWinner = false;
+            //Check win
+            //Horizontals
+            if (pictureBoxTop1.ImageLocation != null && (pictureBoxTop1.ImageLocation == pictureBoxTop2.ImageLocation) && (pictureBoxTop2.ImageLocation == pictureBoxTop3.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "Top");
+            }
+
+            else if (pictureBoxLow1.ImageLocation != null && (pictureBoxLow1.ImageLocation == pictureBoxLow2.ImageLocation) && (pictureBoxLow2.ImageLocation == pictureBoxLow3.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "Bottom");
+            }
+            else if (pictureBoxMid1.ImageLocation != null && (pictureBoxMid1.ImageLocation == pictureBoxMid2.ImageLocation) && (pictureBoxMid2.ImageLocation == pictureBoxMid3.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "H-Center");
+            }
+            //Verticals
+            else if (pictureBoxTop1.ImageLocation != null && (pictureBoxTop1.ImageLocation == pictureBoxMid1.ImageLocation) && (pictureBoxMid1.ImageLocation == pictureBoxLow1.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "Left");
+            }
+            else if (pictureBoxTop2.ImageLocation != null && (pictureBoxTop2.ImageLocation == pictureBoxMid2.ImageLocation) && (pictureBoxMid2.ImageLocation == pictureBoxLow2.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "V-Center");
+            }
+            else if (pictureBoxTop3.ImageLocation != null && (pictureBoxTop3.ImageLocation == pictureBoxMid3.ImageLocation) && (pictureBoxMid3.ImageLocation == pictureBoxLow3.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "Right");
+            }
+            //Diagonals
+            else if (pictureBoxTop1.ImageLocation != null && (pictureBoxTop1.ImageLocation == pictureBoxMid2.ImageLocation) && (pictureBoxMid2.ImageLocation == pictureBoxLow3.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "D1");
+            }
+            else if (pictureBoxTop3.ImageLocation != null && (pictureBoxTop3.ImageLocation == pictureBoxMid2.ImageLocation) && (pictureBoxMid2.ImageLocation == pictureBoxLow1.ImageLocation))
+            {
+                Invoke(new Action(() => fnDisableGame()));
+                MessageBox.Show($"{(bCross ? "Zero" : "Cross")} won", "D2");
+            }
+            else
+            {
+                //Check completion
+                foreach (Control c in this.Controls)
+                {
+                    if (c is PictureBox && (c as PictureBox).ImageLocation == null)
+                    {
+                        bNoWinner = true;
+                        break;
+                    }
+                }
+            }
+
+            //Someone won or moves exhausted
+            if (!bNoWinner)
+            {
+                if (DialogResult.Yes == MessageBox.Show("Play again?", "TicTacToe", MessageBoxButtons.YesNo))
+                {
+                    Invoke(new Action(() => fnClear()));
+                }
+                else
+                {
+                    Invoke(new Action(() => fnDisableGame()));
+                }
+            }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fnClear();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tarun Kapoor - cols_mark@msn.com # fb.me/godofcode", "Created by");
+        }
+    }
+}
